28aecf8d80fb979488a99761a75760d2aa69bab8
Camilo Tello <camilotello04@gmail.com>
2024-05-27 11:49:43 -0500

    Eliminado node_modules del área de preparación

---
+++ app/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Conectar a la base de datos
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/movies', require('./routes/movies'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
